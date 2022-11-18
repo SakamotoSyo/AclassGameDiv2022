99fd9a8e428882e83e58562d55c554f551494a9b
NakamuraTakuto <rulitaku@icloud.com>
2022-11-18 17:44:52 +0900

    [update]円形に広がる弾幕

--- Assets/InGame/Nakamura/Script/BulletControlle.cs
+++ Assets/InGame/Nakamura/Script/BulletControlle.cs
@@ -13,13 +13,13 @@ public class BulletControlle : MonoBehaviour
     float _rightVector = 1;
     float _leftVector = -1;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         _rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void Start()
     {
         if (_bulletDir.GetUp)
         {
