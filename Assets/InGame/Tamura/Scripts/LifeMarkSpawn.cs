9b9dfc020f4eb772fb40b1c32d6af67bf9b8d1cc
CHARA\meich <mei.charaa@gmail.com>
2022-11-18 21:57:03 +0900

    [update]今のライフの分だけライフの画像が出現するようにした [add]WAVE数、ステージクリア条件をどこかから取ってきて反映させるスクリプトを追加

--- Assets/InGame/Tamura/Scripts/LifeMarkSpawn.cs
+++ Assets/InGame/Tamura/Scripts/LifeMarkSpawn.cs
@@ -2,13 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// スタート前の時に、残りライフを何個出すか
-/// </summary>
+/// <summary>スタート前の時に、残りライフを何個出すか</summary>
 public class LifeMarkSpawn : MonoBehaviour
 {
-    [SerializeField, Tooltip("いくつだすか")] int _lifeSpawn;
-    [SerializeField, Header("出したいオブジェクト")] GameObject _life;
+    [SerializeField, Tooltip("いくつだすか")] private　int _lifeSpawn;
+    [SerializeField, Header("出したいオブジェクト")] private GameObject[] _life = new GameObject[2];
 
     void Start()
     {
@@ -18,8 +16,7 @@ public class LifeMarkSpawn : MonoBehaviour
         for(int i = 0; i < _lifeSpawn; i++)
         {
             //今のライフの数だけ、ライフのオブジェクトを生成
-            //生成する場所を変えるやりかたがわからね～～～～
-            Instantiate(_life, gameObject.transform);
+            Instantiate(_life[i], gameObject.transform);
         }
 
     }
