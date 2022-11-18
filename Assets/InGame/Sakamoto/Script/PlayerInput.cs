ad63acfda24f3587fdfad3b172df8d9868c7317e
sakamotosyo <syo10150000@gmail.com>
2022-11-18 15:27:35 +0900

    [Add]PlayerInputの追加

---
+++ Assets/InGame/Sakamoto/Script/PlayerInput.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerInput : MonoBehaviour
+{
+    [Header("自分自身のプレイヤー番号")]
+    [SerializeField] int _playerNum;
+
+    [Tooltip("動く方向")]
+    Vector2 _movement;
+    [Tooltip("現在Action中か")]
+    bool _isAction = false;
+    [Tooltip("Inputをブロックするかどうか")]
+    bool _inputBlock = false;
+
+    /// <summary>現在アクション中かどうか返す</summary>
+    public bool Action
+    {
+        get { return _isAction && !_inputBlock; }
+    }
+    /// <summary>現在の方向入力を返す</summary>
+    public Vector2 MoveInput
+    {
+        get
+        {
+            if (_inputBlock)
+            {
+                return Vector2.zero;
+            }
+            return _movement;
+        }
+    }
+    void Start()
+    {
+       // GameManager.GameStart += InputBlock;
+    }
+
+    void Update()
+    {
+        _movement = new Vector2(Input.GetAxisRaw($"Horizontal"), Input.GetAxisRaw($"Vertical"));
+
+        if (Input.GetButtonDown($"Fire1"))
+        {
+            _isAction = true;
+        }
+        else
+        {
+            _isAction = false;
+        }
+    }
+
+
+    /// <summary>Inputに関する入力を受け付けるかどうか変更する</summary>
+    public void InputBlock()
+    {
+        _inputBlock = !_inputBlock;
+    }
+}
