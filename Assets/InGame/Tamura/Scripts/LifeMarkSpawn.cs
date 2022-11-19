d1a7750f9424696e92470e75016c95fd18347ef9
CHARA\meich <mei.charaa@gmail.com>
2022-11-19 11:55:56 +0900

    [update]残りライフの出現方法を変更

--- Assets/InGame/Tamura/Scripts/LifeMarkSpawn.cs
+++ Assets/InGame/Tamura/Scripts/LifeMarkSpawn.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 /// <summary>スタート前の時に、残りライフを何個出すか</summary>
 public class LifeMarkSpawn : MonoBehaviour
 {
-    [SerializeField, Tooltip("いくつだすか")] private　int _lifeSpawn;
-    [SerializeField, Header("出したいオブジェクト")] private GameObject[] _life = new GameObject[2];
+    [SerializeField,Range(0, 2), Tooltip("いくつだすか")] private　int _lifeCount;
+    [SerializeField, Header("出したいオブジェクト")] private GameObject _life = default;
+    [SerializeField, Header("ライフを出す場所の配列")] private GameObject[] _lifeSpawner = new GameObject[2];
 
     void Start()
     {
         //ライフをゲームマネージャーから取ってくる
         //_lifeSpawn = 
         
-        for(int i = 0; i < _lifeSpawn; i++)
+        for(int i = 0; i < _lifeCount; i++)
         {
             //今のライフの数だけ、ライフのオブジェクトを生成
-            Instantiate(_life[i], gameObject.transform);
+            Instantiate(_life, _lifeSpawner[i].transform);
         }
 
     }
