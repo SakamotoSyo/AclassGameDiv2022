6c08f93a6592e8a4e9213a32f4d2df8883e31672
RAK_kanato <tennbu1@gmail.com>
2022-11-25 12:26:59 +0900

    [fix] タイトル変更とBGM

--- Assets/InGame/Oono/Script/Title/LoadSceneButton.cs
+++ Assets/InGame/Oono/Script/Title/LoadSceneButton.cs
@@ -7,9 +7,25 @@ public class LoadSceneButton : MonoBehaviour
     [SceneName]
     [SerializeField] string _sceneName;
 
+    public AudioClip se;
+    AudioSource audioSource;
+
+    public void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     // Start is called before the first frame update
     public void Ls()
     {
         SceneManagerController.LoadScene(_sceneName);
+    }
+
+    public void Audio()
+    {
+        //AudioSource.PlayClipAtPoint(se, transform.position);
+        audioSource.PlayOneShot(se);
+
+        Invoke("Ls", 1.0f);
     }
 }
